6a8cea322e80b2d33785c8021e48796fa4742a47
MAACJR032 <123107373+MAACJR032@users.noreply.github.com>
2024-02-23 16:14:32 -0300

    Update B.cpp

--- B.cpp
+++ B.cpp
@@ -21,30 +21,8 @@ class Graph
             marked.resize(n, UNVISITED);
             dist.resize(numVertices, inf);
         }
-
-        int first(int v)
-        {
-            if (adj[v].size() == 0)
-                return numVertices;
-            return adj[v].front().first;
-        }
-        int next(int v, int w)
-        {
-            for (int i = 0; i < adj[v].size(); i++)
-            {
-                if ((adj[v][i].first == w) && (i + 1 < adj[v].size()))
-                    return adj[v][i+1].first;
-            }
-            return numVertices;
-        }
         void setEdge(int src, int dest, int weight)
         {
-            if (isEdge(src, dest))
-            {
-                if (adj[src][dest].second < weight)
-                    weight = adj[src][dest].second;
-            }
-            
             adj[src].push_back({dest, weight});
             adj[dest].push_back({src, weight});
         }
@@ -57,15 +35,6 @@ class Graph
             }
             return false;
         }
-        // int weight(int src, int dest)
-        // {
-        //     for (auto edge : adj[src])
-        //     {
-        //         if (edge.first == dest)
-        //             return edge.second;
-        //     }
-        //     return 0;
-        // }
         int getMark(int v)
         {
             return marked[v];
@@ -92,48 +61,38 @@ class Graph
                 do
                 {
                     if (minHeap.empty())
+                    {
+                        cout << "IMPOSSIBLE\n";
                         return;
+                    }
                     
                     v = minHeap.top().first;
-                    minHeap.pop();
-                    
+                    minHeap.pop();                  
                 } while (getMark(v) == VISITED);
                 
                 setMark(v, VISITED);
 
-                for(auto& w : adj[v])
+                for(auto w : adj[v])
                 {
                     if ((getMark(w.first) == UNVISITED) && (dist[w.first] > w.second))
                     {
                         dist[w.first] = w.second;
-                        if (dist[w.first] > range)
-                            range = dist[w.first];
-                        
                         minHeap.push({w.first, dist[w.first]});
                     }
                 }
             }
-            // for (int i = 0; i < dist.size(); i++)
-            //     cout << dist[i];
-            
-            cout << range << '\n';
+            for (int i = 0; i < numVertices; i++)
+            {
+                if (dist[i] > range)
+                    range = dist[i];
+            }
+            if (range == inf)
+                cout << "IMPOSSIBLE\n";
+            else
+                cout << range << '\n';
         }
 };
-/*
-5 7
-0 1 10
-0 2 3
-0 3 20
-1 3 5 
-2 1 2
-2 4 15
-3 4 11
 
-3 3
-0 1 3
-1 2 4
-2 1 5
-*/
 int main()
 {
     int n, m, city1, city2, w;
@@ -157,7 +116,6 @@ int main()
             g.setEdge(city1, city2, w);
         }
         g.Prim();
-        // g.find_min_range();
     }
 
     return 0;
