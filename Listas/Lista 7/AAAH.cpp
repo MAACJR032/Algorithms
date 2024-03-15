697e881c0d7c32a903cdbaa187888a7fbd7e8442
MAACJR032 <123107373+MAACJR032@users.noreply.github.com>
2024-03-15 10:06:49 -0300

    Update AAAH.cpp

--- Listas/Lista 7/AAAH.cpp
+++ Listas/Lista 7/AAAH.cpp
@@ -5,17 +5,20 @@ using namespace std;
 const int moves_row[] = {-2, -2, -1, -1, 1, 1, 2, 2};
 const int moves_col[] = {-1, 1, -2, 2, -2, 2, -1, 1};
 
-bool Is_valid(vector<vector<int>> board, vector<vector<int>> visited, int row, int col, int rows)
+bool Is_valid(vector<vector<int>> board, vector<vector<int>> visited, int row, int col)
 {
-    if ((row >= 0) && (row < rows) && (col >= 0) && (col < 10) && (board[row][col] == 0))
+    if ((row >= 0) && (row < 10) && (col >= 0) && (col < 10) && (board[row][col] == 0))
         return true;
     return false;
 }
 
-bool KnightTour(vector<vector<int>> &board, vector<vector<int>> &visited, int row, int col, int step, int max_moves, int rows)
+bool KnightTour(vector<vector<int>> &board, vector<vector<int>> &visited, int row, int col, int step, int max_moves, int &max_steps)
 {
     if (step == max_moves)
+    {
+
         return true;
+    }
     else
     {
         for (int i = 0; i < 8; i++)
@@ -23,19 +26,18 @@ bool KnightTour(vector<vector<int>> &board, vector<vector<int>> &visited, int ro
             int nextRow = row + moves_row[i];
             int nextCol = col + moves_col[i];
 
-            if (Is_valid(board, visited, nextRow, nextCol, rows))
+            if (Is_valid(board, visited, nextRow, nextCol))
             {
-                step++;
-                board[nextRow][nextCol] = step;
-                visited[nextRow][nextCol]++;
-                if (KnightTour(board, visited, nextRow, nextCol, step, max_moves, rows))
+                step;
+                board[nextRow][nextCol] = step+1;
+                visited[nextRow][nextCol] = 1;
+                if (KnightTour(board, visited, nextRow, nextCol, step+1, max_moves, max_steps))
                     return true;
                 else
                 {
-                    step--;
                     max_moves--;
                     board[nextRow][nextCol] = 0;
-                    visited[nextRow][nextCol]--;
+                    // visited[nextRow][nextCol]--;
                 }
             }    
         }
@@ -49,56 +51,31 @@ int main()
     int cases = 1;   
     while (true)
     {
-        int rows, size = 100;
+        int rows, size = 0;
         cin >> rows;
         if (rows == 0)
             break;
         
-        vector<vector<int>> board(10, vector<int>(10, 0));
+        vector<vector<int>> board(10, vector<int>(10, -1));
 
         for (int i = 0; i < rows; i++)
         {
             int skip, columns;
             cin >> skip >> columns;
 
-            for (int j = 0; j < skip; j++)
+            for (int j = skip; j < skip + columns; j++)
             {
-                board[i][j] = -1;
-                size--;
+                board[i][j] = 0;
             }
-            for (int j = skip + columns; j < 10; j++)
-            {
-                board[i][j] = -1;
-                size--;
-            }
-        }
-        for (int i = rows; i < 10; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                board[i][j] = -1;
-                size--;
-            }
+
+            size += columns;
         }
         
-        pair<int, int> start = {-1, -1};
-        for (int i = 0; i < 10; i++)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                if (board[i][j] == 0)
-                {
-                    start = {i, j};
-                    break;
-                }
-            }
-            if (start.first != -1 && start.second != -1)
-                break;
-        }
-        board[start.first][start.second] = 1;
+        board[0][0] = 1;
+        int max_steps = 0;
         vector<vector<int>> visited = board;
 
-        bool tour = KnightTour(board, visited, start.first, start.second, 1, size, rows);
+        bool tour = KnightTour(board, visited, 0, 0, 1, size, max_steps);
         int squares = 0;
         
         for (int i = 0; i < 10; i++)
