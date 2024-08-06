7a3c06f17e7cd163201ee9e482f88cdbf2ac1694
Márcio Campos Júnior <123107373+MAACJR032@users.noreply.github.com>
2024-08-05 22:56:55 -0300

    added generics and fixed Push_front

--- Algoritmos/Listas/vector.go
+++ Algoritmos/Listas/vector.go
@@ -5,26 +5,32 @@ import (
 	"fmt"
 )
 
-type vector struct {
-	arr []int
+type element interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | 
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+	~float32 | ~float64 | ~string
+}
+
+type vector[T element] struct {
+	arr []T
 	size uint
 	max_size uint
 }
 
 /* Creates an empty vector */
-func Create_vector() *vector {
-	return &vector {arr: make([]int, 10), size: 0, max_size: 10}
+func Create_vector[T element]() *vector[T] {
+	return &vector[T] {arr: make([]T, 10), size: 0, max_size: 10}
 }
 
 /* Creates a vector with specified size and default values */
-func Create_vector_size(length uint) *vector {
-	return &vector {arr: make([]int, length, length * 2), size: length, max_size: length * 2}
+func Create_vector_size[T element](length uint) *vector[T] {
+	return &vector[T] {arr: make([]T, length, length * 2), size: length, max_size: length * 2}
 }
 
 /* Creates a vector with specified size and filled with the given element */
-func Create_vector_elem(length uint, elem int) *vector {
-	vect := &vector {
-		arr: make([]int, length * 2),
+func Create_vector_elem[T element](length uint, elem T) *vector[T] {
+	vect := &vector[T] {
+		arr: make([]T, length * 2),
 		size: length,
 		max_size: length * 2,
 	}
@@ -37,9 +43,9 @@ func Create_vector_elem(length uint, elem int) *vector {
 }
 
 /* Creates a copy of another vector */
-func Create_vector_copy(v *vector) *vector {
-	vect := &vector{
-		arr: make([]int, v.size, v.max_size),
+func Create_vector_copy[T element](v *vector[T]) *vector[T] {
+	vect := &vector[T] {
+		arr: make([]T, v.size, v.max_size),
 		size: v.size,
 		max_size: v.max_size,
 	}
@@ -50,8 +56,8 @@ func Create_vector_copy(v *vector) *vector {
 
 
 /* Copies another vector's contents into this one */
-func (v *vector) Copy_vector(other *vector) {
-	v.arr = make([]int, other.size, other.max_size)
+func (v *vector[T]) Copy_vector(other *vector[T]) {
+	v.arr = make([]T, other.size, other.max_size)
 	copy(v.arr, other.arr)
 
 	v.size = other.size
@@ -60,10 +66,11 @@ func (v *vector) Copy_vector(other *vector) {
 
 
 /* Private function to double the vector size if it is full */
-func check_realloc(v *vector) {
+func check_realloc[T element] (v *vector[T]) {
+
 	if v.size == v.max_size {
 		v.max_size = v.max_size * 2
-		new_arr := make([]int, v.max_size)
+		new_arr := make([]T, v.max_size)
 		copy(new_arr, v.arr)
 		v.arr = new_arr
 	}
@@ -71,26 +78,28 @@ func check_realloc(v *vector) {
 
 
 /* Insets an element to the end of the vector */
-func (v *vector) Push_back(elem int) {
+func (v *vector[T]) Push_back(elem T) {
 	check_realloc(v)
 	v.arr[v.size] = elem
 	v.size++
 }
 
 /* Insets an element to the begining of the vector */
-func (v *vector) Push_front(elem int) {
+func (v *vector[T]) Push_front(elem T) {
 	check_realloc(v)
-	// temp := make([]int, v.size + 1, v.max_size)
 	
-	copy(v.arr[1:], v.arr[:v.size])
-	v.arr[0] = elem
+	temp := make([]T, v.size + 1, v.max_size)
+	copy(temp[1:], v.arr)
+	temp[0] = elem
+
+	v.arr = temp
 	v.size++
 }
 
 /* Removes the last element of the vector */
-func (v *vector) Pop_back() (int, error) {
+func (v *vector[T]) Pop_back() (T, error) {
 	if v.Empty() {
-		return -1, errors.New("vector is empty")
+		return *new(T), errors.New("vector is empty")
 	}
 
 	elem := v.arr[v.size-1]
@@ -99,9 +108,9 @@ func (v *vector) Pop_back() (int, error) {
 }
 
 /* Removes the first element of the vector */
-func (v *vector) Pop_front() (int, error) {
+func (v *vector[T]) Pop_front() (T, error) {
 	if v.Empty() {
-		return -1, errors.New("vector is empty")
+		return *new(T), errors.New("vector is empty")
 	}
 
 	elem := v.arr[0]
@@ -110,7 +119,7 @@ func (v *vector) Pop_front() (int, error) {
 	return elem, nil
 }
 
-func (v *vector) Erase(start uint, end uint) error {
+func (v *vector[T]) Erase(start uint, end uint) error {
 	if start >= v.size || end > v.size || end < start {
 		return errors.New("indexes out of range")
 	}
@@ -122,7 +131,7 @@ func (v *vector) Erase(start uint, end uint) error {
 
 
 /* Assigns elem to the vector at the given index */
-func (v *vector) Assign_at(elem int, index uint) (error) {
+func (v *vector[T]) Assign_at(elem T, index uint) (error) {
 	if index >= v.size {
 		return errors.New("index out of range")
 	}
@@ -131,22 +140,23 @@ func (v *vector) Assign_at(elem int, index uint) (error) {
 	return nil
 }
 
+
 /* Getters */
-func (v *vector) Size() uint {
+func (v *vector[T]) Size() uint {
 	return v.size
 }
 
-func (v *vector) Capacity() uint {
+func (v *vector[T]) Capacity() uint {
 	return v.max_size
 }
 
-func (v *vector) Empty() bool {
+func (v *vector[T]) Empty() bool {
 	return (v.size == 0)
 }
 
-func (v *vector) At(index uint) (int, error) {
+func (v *vector[T]) At(index uint) (T, error) {
 	if index >= v.size{
-		return 0, errors.New("index out of range")
+		return *new(T), errors.New("index out of range")
 	}
 
 	return v.arr[index], nil
@@ -154,15 +164,15 @@ func (v *vector) At(index uint) (int, error) {
 
 
 /* Non-return functions */
-func (v *vector) Clear() {
+func (v *vector[T]) Clear() {
 	v.size = 0
 	v.max_size = 10
-	v.arr = make([]int, 10)
+	v.arr = make([]T, 10)
 }
 
-func (v *vector) Print_vector() {
+func (v *vector[T]) Print_vector() {
 	for i := 0; i < int(v.size); i++ {
-		fmt.Printf("%d ", v.arr[i])
+		fmt.Printf("%v ", v.arr[i])
 	}
 	fmt.Println()
 }
