cb3d39d1ab093a4dc6809bbf2f2bd3cb776b95c4
Márcio Campos Júnior <123107373+MAACJR032@users.noreply.github.com>
2024-08-07 18:17:00 -0300

    Fixed In_list function and changed Push_back_list and Push_front_list
    Now each node has a method to what list does it belong to. Push_list functions don't change the other list, instead they just copy their elements, thus it's slower than previously.
    
    TODO: create other Push list functions for when you don't mind clearing the other list

--- Algoritmos/Listas/doubly_linked_list.go
+++ Algoritmos/Listas/doubly_linked_list.go
@@ -3,15 +3,21 @@ package list
 import (
 	"errors"
 	"fmt"
-	"sync"
 )
 
 type node struct  {
 	elem any
 	next *node
 	prev *node
+	list *List
 }
 
+// Returns a new and initialized node
+func New_node(elem any, next *node, prev *node, list *List) *node {
+	return &node {elem, next, prev, list}
+}
+
+// Returns the next node or nil
 func (n *node) Next() *node {
 	if n == nil {
 		return nil
@@ -19,6 +25,7 @@ func (n *node) Next() *node {
 	return n.next
 }
 
+// Returns the previous node or nil
 func (n *node) Prev() *node {
 	if n == nil {
 		return nil
@@ -26,111 +33,61 @@ func (n *node) Prev() *node {
 	return n.prev
 }
 
-type list struct {
+type List struct {
 	size uint
 	head *node
 	tail *node
 }
 
-// Constructors
-func New_node(elem any, next *node, prev *node) *node {
-	return &node {elem, next, prev}
-}
-
-// Returns a new empty list
-func New_list() *list {
-	l := list {
+// Returns a new and initialized list
+func New_list() *List {
+	l := &List {
 		size: 0,
-		head: New_node(*new(any), nil, nil),
-		tail: New_node(*new(any), nil, nil),
+		head: nil,
+		tail: nil,
 	}
 
+	l.head = New_node(*new(any), nil, nil, l)
+	l.tail = New_node(*new(any), nil, nil, l)
+	
 	l.head.next = l.tail
 	l.tail.prev = l.head
 
-	return &l
+	return l
 }
 
-// Checks if the specified node is in the list
-func (l *list) In_list(n *node) bool {
-	for i := l.Begin(); i != l.End(); i = i.next {
-		if i == n {
-			return true
-		}
-	}
-
-	return false
+// Checks if the node 'n' is in the list
+func (l *List) In_list(n *node) bool {
+	return n.list == l
 }
 
-func (l *list) in_list(n *node, result_chan chan<- bool, wg *sync.WaitGroup) {
-	for i := l.Begin(); i != l.End(); i = i.next {
-		if i == n {
-			result_chan <- true
-			wg.Done()
-			return
-		}
-	}
-
-	result_chan <- false
-	wg.Done()
-}
-
-/* Getters */
-func (l *list) Begin() *node {
+// Returns the first node of the list
+func (l *List) Begin() *node {
 	return l.head.next
 }
 
-func (l *list) End() *node {
-	return l.tail
-}
-
-func (l *list) R_begin() *node {
+// Returns the last node of the list
+func (l *List) End() *node {
 	return l.tail.prev
 }
 
-func (l *list) R_end() *node {
-	return l.head
-}
-
-func (l *list) Empty() bool {
+// Returns true if the list is empty, otherwise, it will return false
+func (l *List) Empty() bool {
 	return l.head.next == l.tail 
 }
 
-/* Insertion functions */
-
 // Inserts the element in the end of the list
-func (l *list) Push_back(elem any) {
-	n := New_node(elem, l.tail, l.tail.prev)
+func (l *List) Push_back(elem any) {
+	n := New_node(elem, l.tail, l.tail.prev, l)
 
 	n.prev.next = n
 	l.tail.prev = n
-
 	l.size++
 }
 
-// Inserts the other list in the end of this list
-func (l *list) Push_back_list(other *list) error {
-	if other.size == 0 {
-		return nil
-	} else if other == nil {
-		return errors.New("other list is nil")
-	}
-	
-	l.End().prev.next = other.Begin()
-	other.Begin().prev = l.End().prev
-
-	l.tail.prev = other.End().prev
-	other.End().prev.next = l.tail
-
-	other.head.next = other.tail
-	other.tail.prev = other.head
-
-	return nil
-}
-
 // Inserts the element in the begining of the list
-func (l *list) Push_front(elem any) {
-	n := New_node(elem, l.head.next, l.head)
+func (l *List) Push_front(elem any) {
+	n := New_node(elem, l.head.next, l.head, l)
 
 	n.next.prev = n
 	l.head.next = n
@@ -138,109 +95,125 @@ func (l *list) Push_front(elem any) {
 	l.size++
 }
 
-// Inserts the other list in the begining of this list
-func (l *list) Push_front_list(other *list) error {
+// Inserts a copy of another list at the back of list l.
+func (l *List) Push_back_list(other *List) error {
 	if other.size == 0 {
 		return nil
 	} else if other == nil {
 		return errors.New("other list is nil")
 	}
+	
+	var n *node = other.Begin()
+	for n != other.tail {
+		l.Push_back(n.elem)
+		n = n.next
+	}
 
-	l.Begin().prev = other.End().prev
-	other.End().prev.next = l.Begin()
+	return nil
+}
 
-	l.head.next = other.Begin()
-	other.Begin().prev = l.head
-
-	other.head.next = other.tail
-	other.tail.prev = other.head
+// Inserts a copy of another list at the front of list l.
+func (l *List) Push_front_list(other *List) error {
+	if other.size == 0 {
+		return nil
+	} else if other == nil {
+		return errors.New("other list is nil")
+	}
+	
+	var n *node = other.End()
+	for n != other.head {
+		l.Push_front(n.elem)
+		n = n.prev
+	}
 
 	return nil
 }
 
 // Inserts the element after the specified node
-func (l *list) Insert_after(elem any, n *node) {
-	if n == l.tail {
-		return
-	} else if !l.In_list(n) {
+func (l *List) Insert_after(elem any, n *node) {
+	if n == l.tail || !l.In_list(n) {
 		return
 	}
 	
-	new_node := New_node(elem, n.next, n)
+	new_node := New_node(elem, n.next, n, l)
 	n.next.prev = new_node
 	n.next = new_node
 }
 
 // Inserts the element before the specified node
-func (l *list) Insert_before(elem any, n *node) {
-	if n == l.head {
-		return
-	} else if !l.In_list(n) {
+func (l *List) Insert_before(elem any, n *node) {
+	if n == l.head || !l.In_list(n) {
 		return
 	}
 	
-	new_node := New_node(elem, n, n.prev)
+	new_node := New_node(elem, n, n.prev, l)
 	n.prev.next = new_node
 	n.prev = new_node
 }
 
-/* Remove functions*/
-
 // Removes the last element of the list
-func (l *list) Pop_back() (any, error) {
-
+func (l *List) Pop_back() (any, error) {
 	if l.Empty() {
 		return *new(any), errors.New("list is empty")
 	}
 	
-	elem := l.tail.prev.elem
+	n := l.tail.prev
 
 	l.tail.prev = l.tail.prev.prev
 	l.tail.prev.next = l.tail
-
 	l.size--
-	return elem, nil
+
+	n.next = nil
+	n.prev = nil
+
+	return n.elem, nil
 }
 
 // Removes the first node of the list
-func (l *list) Pop_front() (any, error) {
+func (l *List) Pop_front() (any, error) {
 
     if l.Empty() {
         return *new(any), errors.New("list is empty")
     }
 
-    val := l.head.next.elem
+    n := l.head.next
 
     l.head.next = l.head.next.next
     l.head.next.prev = l.head
-    
 	l.size--
-    return val, nil
+
+	n.next = nil
+	n.prev = nil
+
+    return n.elem, nil
 }
 
 // Removes the specified node of the list
-func (l *list) Pop_node(n *node) error {
+func (l *List) Pop_node(n *node) (*node, error) {
 
 	if n == l.head || n == l.tail {
-		return errors.New("especified node is out of range")
+		return nil, errors.New("node is out of range")
 	} else if n == nil {
-		return errors.New("especified node is nil")
+		return nil, errors.New("node is nil")
 	} else if !l.In_list(n) {
-		return nil
+		return nil, nil
 	}
 
 	n.prev.next = n.next
 	n.next.prev = n.prev
 	l.size--
 
-	return nil
+	n.next = nil
+	n.prev = nil
+	n.list = nil
+
+	return n, nil
 }
 
 // Removes all ocurrences of elem
-func (l *list) Remove(elem any) {
+func (l *List) Remove(elem any) {
 
 	for n := l.Begin(); n != l.End(); {
-
 		if n.elem == elem {
 			n = n.next
 			l.Pop_node(n.prev)
@@ -251,38 +224,18 @@ func (l *list) Remove(elem any) {
 }
 
 // Removes all the elements of the list
-func (l *list) Clear() {
-	for l.size > 0 {
-		l.Pop_front()
-	}
+func (l *List) Clear() {
+	l.head.next = l.tail
+	l.tail.prev = l.head
+	l.size = 0
 }
 
 /* Move functions */
 
 // Moves the e node to after n node
-func (l *list) Move_after(e *node, n *node) {
-
-	if e == n || e == l.head || e == l.tail || n == l.tail {
-		return
-	}
-
-	var wg sync.WaitGroup
-	chan_result_e := make(chan bool, 1)
-	chan_result_n := make(chan bool, 1)
-	
-	wg.Add(2)
-
-	go l.in_list(e, chan_result_e, &wg)
-	go l.in_list(n, chan_result_n, &wg)
-
-	wg.Wait()
-	close(chan_result_e)
-	close(chan_result_n)
-
-	result_e := <- chan_result_e
-	result_n := <- chan_result_n
-
-	if !result_e || !result_n {
+func (l *List) Move_after(e *node, n *node) {
+	if e == n || e == l.head || e == l.tail || 
+	   n == l.tail || !l.In_list(e) || !l.In_list(n) {
 		return
 	}
 
@@ -297,30 +250,9 @@ func (l *list) Move_after(e *node, n *node) {
 }
 
 // Moves the e node to before n node
-func (l *list) Move_before(e *node, n *node) {
-
-	if e == n || e == l.head || e == l.tail || n == l.head {
-		return
-	}
-
-	var wg sync.WaitGroup
-	chan_result_e := make(chan bool, 1)
-	chan_result_n := make(chan bool, 1)
-	
-	wg.Add(2)
-
-	go l.in_list(e, chan_result_e, &wg)
-	go l.in_list(n, chan_result_n, &wg)
-
-	wg.Wait()
-	close(chan_result_e)
-	close(chan_result_n)
-
-	result_e := <- chan_result_e
-	result_n := <- chan_result_n
-
-	if !result_e || !result_n {
-		fmt.Println("Num ta")
+func (l *List) Move_before(e *node, n *node) {
+	if e == n || e == l.head || e == l.tail ||
+	   n == l.head || !l.In_list(e) || !l.In_list(n) {
 		return
 	}
 
@@ -335,9 +267,9 @@ func (l *list) Move_before(e *node, n *node) {
 }
 
 // Output list
-func (l *list) Print_list() {
+func (l *List) Print_list() {
 
-	for n := l.Begin(); n != l.End(); n = n.next {
+	for n := l.Begin(); n != l.tail; n = n.next {
 		fmt.Printf("%v ", n.elem)
 	}
 	fmt.Println()
